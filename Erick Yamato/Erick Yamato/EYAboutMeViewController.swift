3f52098894df59ad9d398e6ba00ef3884ecf2cef
Erick Yamato <erick.yamato@me.com>
2019-03-13 15:29:12 -0300

    renaming files

--- Erick Yamato/Erick Yamato/EYAboutMeViewController.swift
+++ Erick Yamato/Erick Yamato/EYAboutMeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  EYAboutMeViewController.swift
 //  Erick Yamato
 //
 //  Created by Erick Mitsugui Yamato on 19/12/18.
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class EYAboutMeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
